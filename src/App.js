6a86dafa80a19537497cf3a399f001bf0f163fd8
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-16 14:01:26 +0300

    adding 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/header.component";
 import Footer from "./components/footer/footer.component";
 import Home from './routes/home/home.component';
 import AuthorPage from './routes/authorPage/authorPage.component';
+import NotFoundPage from './routes/notFoundPage/notFound.component';
 import { QuoteProvider } from './context/quote.context';
 import { AuthorProvider } from './context/author.context'
 
@@ -13,7 +14,8 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/:authorName' element={<AuthorProvider><AuthorPage/></AuthorProvider>}/>
+          <Route path='/author/:authorName' element={<AuthorProvider><AuthorPage/></AuthorProvider>}/>
+          <Route path="/*" element={<NotFoundPage/>}/>
         </Routes>
         <Footer/>
       </div>
